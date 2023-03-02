35aff0d1c0a90ef5660699fe00867a367c92a9e8
MayankBorkar <mayankborkar190@gmail.com>
2023-03-02 15:33:11 +0530

    changes done.

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,61 @@
+import {FlatList, StyleSheet, View} from "react-native";
+import {CATEGORIES, MEALS} from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+import {useLayoutEffect} from "react";
+
+
+function MealsOverviewScreen({route, navigation}) {
+    const catId = route.params.categoryId;
+
+    const displayMeals = MEALS.filter((mealsItem) => {
+        return mealsItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    useLayoutEffect(() => {
+
+        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+        navigation.setOptions({
+            title: categoryTitle
+        });
+
+    }, [catId, navigation]);
+
+
+    function renderMealItem(itemData) {
+        const item = itemData.item;
+
+        const mealItemProps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration,
+        }
+
+        return (
+            <MealItem {...mealItemProps}/>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={displayMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
+        </View>
+    )
+
+}
+
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16
+    },
+})
